72bb002b2f39c39d69a0b72126e2174c01c727cb
Paul Chobert <paul@chobert.fr>
2012-10-06 23:09:37 +0200

    turn into a rack app

--- app.rb
+++ app.rb
@@ -1,20 +1,24 @@
 require 'rubygems'
+require 'bundler/setup'
 
 require 'sinatra'
 require 'haml'
 require 'sass'
+require 'pry'
 
-if ENV['PASSWORD']
-  use Rack::Auth::Basic do |username, password|
-    password == ENV['PASSWORD']
+class App < Sinatra::Application
+  if ENV['PASSWORD']
+    use Rack::Auth::Basic do |username, password|
+      password == ENV['PASSWORD']
+    end
   end
-end
 
-get '/' do
-  haml :index
-end
+  get '/' do
+    haml :index
+  end
 
-get '/styles.css' do
-  content_type 'text/css; charset=utf-8'
-  sass :styles
+  get '/styles.css' do
+    content_type 'text/css; charset=utf-8'
+    sass :styles
+  end
 end
